868e5d77af1d0c06e5a0e57679703b7f4b22a47e
Yahialabeeb <yahia.labeeb@gmail.com>
2021-12-05 17:19:50 +0200

    finish

--- snacks_project/snacks_project/settings.py
+++ snacks_project/snacks_project/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'snacks_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'snacks_project.wsgi.application'
 
